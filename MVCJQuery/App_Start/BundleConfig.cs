dbc9f81dbe8900693c0f4b02a7a33388ec3993df
wjkong <wjkonger@gmail.com>
2018-11-23 08:21:18 -0500

    Complete Hello Application

--- MVCJQuery/App_Start/BundleConfig.cs
+++ MVCJQuery/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace MVCJQuery
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
                       "~/Content/site.css"));
         }
     }
